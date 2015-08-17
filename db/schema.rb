26def7b91a03d8d2ef72673a4750a9b0050faa55
myun2 <myun2@nwhite.info>
2015-08-17 16:06:48 +0900

    add user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817065639) do
+ActiveRecord::Schema.define(version: 20150817070324) do
 
   create_table "memos", force: :cascade do |t|
     t.string   "memo"
@@ -39,9 +39,11 @@ ActiveRecord::Schema.define(version: 20150817065639) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name",                   default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "youtubes", force: :cascade do |t|
